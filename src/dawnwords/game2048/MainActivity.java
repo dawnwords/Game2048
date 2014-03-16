1749921e98369d65c41516f8addf4cb167658852
Jingxiao Gu <gujingxiao@gmail.com>
2014-03-16 14:37:26 +0800

    Finish Generating Cell Logic

--- src/dawnwords/game2048/MainActivity.java
+++ src/dawnwords/game2048/MainActivity.java
@@ -1,129 +1,141 @@
 package dawnwords.game2048;
 
-import java.util.LinkedList;
-import java.util.List;
-
-import dawnwords.game2048.core.CalculateThread;
-import dawnwords.game2048.core.Direction;
-import dawnwords.game2048.view.Cell;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
+import dawnwords.game2048.core.CalculateThread;
+import dawnwords.game2048.core.Direction;
+import dawnwords.game2048.view.Cell;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
-	public static final int ACTION_END = 1;
-	public static final int SHOW_END = 2;
-	public static final int CALCULATE_END = 3;
-	public static final int BOARD_SIZE = 4;
+    public static final int ACTION_END = 1;
+    public static final int SHOW_END = 2;
+    public static final int CALCULATE_END = 3;
+    public static final int GENERATE_CELL = 4;
 
-	private final Handler handler = new Handler() {
-		private List<Cell> cellList, updatedList;
+    public static final int BOARD_SIZE = 4;
 
-		@Override
-		public void handleMessage(Message msg) {
-			synchronized (this) {
-				switch (msg.what) {
-				case ACTION_END:
-					Cell c = (Cell) msg.obj;
-					cellList.remove(c);
+    public static final String DEBUG_TAG = "Game2048";
 
-					if (c.shouldMerge()) {
-						boolean exists = false;
-						for (Cell cell : updatedList) {
-							if (cell.x() == c.moveX() && cell.y() == c.moveY()) {
-								exists = true;
-								break;
-							}
-						}
+    private final Handler handler = new Handler() {
+        private List<Cell> cellList;
+        private List<Cell> updatedList;
 
-						System.out.printf("Merged %s\n", c);
-						wrapper.removeView(c);
-						if (!exists) {
-							c = new Cell(MainActivity.this, c.moveX(),
-									c.moveY(), c.value() * 2, this);
-							wrapper.addView(c);
-							c.show();
-							updatedList.add(c);
-						}
-					} else {
-						System.out.printf("Move %s\n", c);
-						c.updatePosition(c.moveX(), c.moveY());
-						updatedList.add(c);
-					}
+        @Override
+        public void handleMessage(Message msg) {
+            synchronized (this) {
+                switch (msg.what) {
+                    case ACTION_END:
+                        Cell c = (Cell) msg.obj;
+                        cellList.remove(c);
 
-					if (cellList.size() == 0) {
-						board = new Cell[BOARD_SIZE][BOARD_SIZE];
-						for (Cell cell : updatedList) {
-							board[cell.x()][cell.y()] = cell;
-						}
-					}
-					break;
-				case SHOW_END:
-					break;
-				case CALCULATE_END:
-					cellList = (List<Cell>) msg.obj;
-					updatedList = new LinkedList<Cell>();
-					for (int x = 0; x < BOARD_SIZE; x++) {
-						for (int y = 0; y < BOARD_SIZE; y++) {
-							if (board[x][y] != null) {
-								board[x][y].performAction();
-							}
-						}
-					}
-					break;
-				default:
-					break;
-				}
-			}
-		}
+                        if (c.shouldMerge()) {
+                            boolean exists = false;
+                            for (Cell cell : updatedList) {
+                                if (cell.x() == c.moveX() && cell.y() == c.moveY()) {
+                                    exists = true;
+                                    break;
+                                }
+                            }
 
-	};
+                            Log.i(DEBUG_TAG, "Merge " + c);
+                            wrapper.removeView(c);
+                            if (!exists) {
+                                c = new Cell(MainActivity.this, c.moveX(),
+                                        c.moveY(), c.value() * 2, this);
+                                wrapper.addView(c);
+                                c.show();
+                                updatedList.add(c);
+                            }
+                        } else {
+                            Log.i(DEBUG_TAG, "Move " + c);
+                            c.updatePosition(c.moveX(), c.moveY());
+                            updatedList.add(c);
+                        }
 
-	private Cell[][] board;
-	private RelativeLayout wrapper;
+                        if (cellList.size() == 0) {
+                            board = new Cell[BOARD_SIZE][BOARD_SIZE];
+                            for (Cell cell : updatedList) {
+                                board[cell.x()][cell.y()] = cell;
+                            }
+                            sendEmptyMessage(GENERATE_CELL);
+                        }
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+                        break;
+                    case SHOW_END:
+                        break;
+                    case CALCULATE_END:
+                        cellList = (List<Cell>) msg.obj;
+                        updatedList = new LinkedList<Cell>();
+                        for (int x = 0; x < BOARD_SIZE; x++) {
+                            for (int y = 0; y < BOARD_SIZE; y++) {
+                                if (board[x][y] != null) {
+                                    board[x][y].performAction();
+                                }
+                            }
+                        }
 
-		wrapper = (RelativeLayout) findViewById(R.id.wrapper);
+                        break;
+                    case GENERATE_CELL:
+                        int availableCount = BOARD_SIZE * BOARD_SIZE - (updatedList == null ? 0 : updatedList.size());
+                        int randomIndex = (int) (Math.random() * availableCount - 1);
+                        for (int i = 0; i < BOARD_SIZE; i++) {
+                            for (int j = 0; j < BOARD_SIZE; j++) {
+                                if (board[i][j] == null && randomIndex-- == 0) {
+                                    int value = Math.random() < 0.9 ? 2 : 4;
+                                    board[i][j] = new Cell(MainActivity.this, i,
+                                            j, value, this);
+                                    wrapper.addView(board[i][j]);
+                                    board[i][j].show();
+                                }
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    };
 
-		final int[][] cells = { { 8, 0, 8, 8 }, { 8, 8, 0, 8 }, { 8, 0, 0, 8 },
-				{ 8, 0, 0, 8 } };
-		board = new Cell[BOARD_SIZE][BOARD_SIZE];
-		for (int x = 0; x < BOARD_SIZE; x++) {
-			for (int y = 0; y < BOARD_SIZE; y++) {
-				if (cells[x][y] != 0) {
-					board[x][y] = new Cell(this, x, y, cells[x][y], handler);
-					wrapper.addView(board[x][y]);
-					board[x][y].show();
-				}
-			}
-		}
-	}
+    private Cell[][] board;
+    private RelativeLayout wrapper;
 
-	public void right(View v) {
-		algorithm(Direction.RIGHT);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-	public void left(View v) {
-		algorithm(Direction.LEFT);
-	}
+        wrapper = (RelativeLayout) findViewById(R.id.wrapper);
+        board = new Cell[BOARD_SIZE][BOARD_SIZE];
+        handler.sendEmptyMessage(GENERATE_CELL);
+    }
 
-	public void up(View v) {
-		algorithm(Direction.UP);
-	}
+    public void right(View v) {
+        algorithm(Direction.RIGHT);
+    }
 
-	public void down(View v) {
-		algorithm(Direction.DOWN);
-	}
+    public void left(View v) {
+        algorithm(Direction.LEFT);
+    }
 
-	private void algorithm(Direction direction) {
-		new CalculateThread(board, direction, handler).start();
-	}
+    public void up(View v) {
+        algorithm(Direction.UP);
+    }
+
+    public void down(View v) {
+        algorithm(Direction.DOWN);
+    }
+
+    private void algorithm(Direction direction) {
+        new CalculateThread(board, direction, handler).start();
+    }
 }
